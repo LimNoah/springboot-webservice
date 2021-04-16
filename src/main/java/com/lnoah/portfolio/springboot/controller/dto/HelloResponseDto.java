8aff8066b0166f5db35cbab819a1577140f1a659
LNoah <limnoah0301@gmail.com>
2021-04-16 17:49:34 +0900

    test: Add ResponseDto class.
    Add HelloResponseDto class.

---
+++ src/main/java/com/lnoah/portfolio/springboot/controller/dto/HelloResponseDto.java
@@ -0,0 +1,12 @@
+package com.lnoah.portfolio.springboot.controller.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class HelloResponseDto {
+    private final String name;
+    private final int amount;
+}
