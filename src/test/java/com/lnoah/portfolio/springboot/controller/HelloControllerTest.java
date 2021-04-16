9a992e32aa3f08be2737cadeb03a99892c6c311b
LNoah <limnoah0301@gmail.com>
2021-04-16 17:07:38 +0900

    test: Add controller Test class.
    Add HelloControllerTest class for test.

---
+++ src/test/java/com/lnoah/portfolio/springboot/controller/HelloControllerTest.java
@@ -0,0 +1,30 @@
+package com.lnoah.portfolio.springboot.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(controllers = HelloController.class)
+class HelloControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @DisplayName("HelloTest")
+    @Test
+    void hello() throws Exception {
+        String hello = "hello";
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/hello"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(hello));
+    }
+}
