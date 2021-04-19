bfb23087f022df05d61939f585cba2a42d855a05
LNoah <limnoah0301@gmail.com>
2021-04-19 15:59:12 +0900

    test: Add Dto API's test.
    Add helloDto API's test at HelloControllerTest class.

--- src/test/java/com/lnoah/portfolio/springboot/controller/HelloControllerTest.java
+++ src/test/java/com/lnoah/portfolio/springboot/controller/HelloControllerTest.java
@@ -1,5 +1,6 @@
 package com.lnoah.portfolio.springboot.controller;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
@@ -8,8 +9,12 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(controllers = HelloController.class)
@@ -17,6 +22,14 @@ class HelloControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @BeforeEach
+    void before(WebApplicationContext wac) {
+        mockMvc = MockMvcBuilders.webAppContextSetup(wac)
+                .addFilter(new CharacterEncodingFilter("UTF-8", true))
+                .alwaysDo(print())
+                .build();
+    }
 
     @DisplayName("HelloTest")
     @Test
@@ -26,5 +39,17 @@ class HelloControllerTest {
         mockMvc.perform(MockMvcRequestBuilders.get("/hello"))
                 .andExpect(status().isOk())
                 .andExpect(content().string(hello));
+    }
+
+    @DisplayName("HelloDto 테스트")
+    @Test
+    void helloDto() throws Exception {
+        String name = "hello";
+        int amount = 20000;
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/hello/dto").param("name", name).param("amount", String.valueOf(amount)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name").value(name))
+                .andExpect(jsonPath("$.amount").value(amount));
     }
 }
