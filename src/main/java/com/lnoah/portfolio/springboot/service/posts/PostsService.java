b7086f43de0bcc8686599aaefd06b40c8adb9bca
LNoah <limnoah0301@gmail.com>
2021-05-27 02:04:28 +0900

    feat: Update PostsService.
    Add update, findById method

--- src/main/java/com/lnoah/portfolio/springboot/service/posts/PostsService.java
+++ src/main/java/com/lnoah/portfolio/springboot/service/posts/PostsService.java
@@ -3,9 +3,12 @@ package com.lnoah.portfolio.springboot.service.posts;
 import com.lnoah.portfolio.springboot.controller.dto.PostsResponseDto;
 import com.lnoah.portfolio.springboot.controller.dto.PostsSaveRequestDto;
 import com.lnoah.portfolio.springboot.controller.dto.PostsUpdateRequestDto;
+import com.lnoah.portfolio.springboot.exception.ResourceNotFoundException;
+import com.lnoah.portfolio.springboot.model.posts.Posts;
 import com.lnoah.portfolio.springboot.repository.posts.PostsRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
@@ -17,11 +20,17 @@ public class PostsService {
         return postsRepository.save(requestDto.toEntity()).getId();
     }
 
-    public PostsResponseDto findById(Long id) {
-        return null;
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts =  postsRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("해당 게시글이 없습니다. id = " + id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
     }
 
-    public Long update(Long id, PostsUpdateRequestDto requestDto) {
-        return 0L;
+    public PostsResponseDto findById(Long id) {
+        Posts posts =  postsRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("해당 게시글이 없습니다. id = " + id));
+        return new PostsResponseDto(posts);
     }
 }
