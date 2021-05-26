06c406918392791c9355d557c5b5f466d5c1dd0a
LNoah <limnoah0301@gmail.com>
2021-05-26 21:28:42 +0900

    feat: Update PostsApiController
    Add update / findById API.

--- src/main/java/com/lnoah/portfolio/springboot/controller/PostsApiController.java
+++ src/main/java/com/lnoah/portfolio/springboot/controller/PostsApiController.java
@@ -1,11 +1,12 @@
 package com.lnoah.portfolio.springboot.controller;
 
+import com.lnoah.portfolio.springboot.controller.dto.PostsResponseDto;
 import com.lnoah.portfolio.springboot.controller.dto.PostsSaveRequestDto;
+import com.lnoah.portfolio.springboot.controller.dto.PostsUpdateRequestDto;
 import com.lnoah.portfolio.springboot.service.posts.PostsService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -13,7 +14,17 @@ public class PostsApiController {
     private final PostsService postsService;
 
     @PostMapping("/api/v1/posts")
-    public Long save(@RequestBody PostsSaveRequestDto requestDto) {
-        return postsService.save(requestDto);
+    public ResponseEntity<Long> save(@RequestBody PostsSaveRequestDto requestDto) {
+        return ResponseEntity.ok(postsService.save(requestDto));
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public ResponseEntity<Long> update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto){
+        return ResponseEntity.ok(postsService.update(id, requestDto));
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public ResponseEntity<PostsResponseDto> findById(@PathVariable Long id){
+        return ResponseEntity.ok(postsService.findById(id));
     }
 }
