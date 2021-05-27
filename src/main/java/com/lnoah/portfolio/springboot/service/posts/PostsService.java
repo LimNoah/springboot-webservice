5e944970534d5979f75c0ba468dee4869eda20d6
LNoah <limnoah0301@gmail.com>
2021-05-27 22:37:25 +0900

    feat: Update PotsService.java
    Add delete method for use delete API.

--- src/main/java/com/lnoah/portfolio/springboot/service/posts/PostsService.java
+++ src/main/java/com/lnoah/portfolio/springboot/service/posts/PostsService.java
@@ -1,5 +1,6 @@
 package com.lnoah.portfolio.springboot.service.posts;
 
+import com.lnoah.portfolio.springboot.controller.dto.PostsListResponseDto;
 import com.lnoah.portfolio.springboot.controller.dto.PostsResponseDto;
 import com.lnoah.portfolio.springboot.controller.dto.PostsSaveRequestDto;
 import com.lnoah.portfolio.springboot.controller.dto.PostsUpdateRequestDto;
@@ -9,6 +10,9 @@ import com.lnoah.portfolio.springboot.repository.posts.PostsRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -32,5 +36,16 @@ public class PostsService {
     public PostsResponseDto findById(Long id) {
         Posts posts =  postsRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("해당 게시글이 없습니다. id = " + id));
         return new PostsResponseDto(posts);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream().map(PostsListResponseDto::new).collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete(Long id){
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("해당 게시글이 없습니다. id = " + id));
+        postsRepository.delete(posts);
     }
 }
