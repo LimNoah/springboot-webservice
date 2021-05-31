1fbecaa9beee10ab32f057682ba3b01f1f7e58a6
LNoah <limnoah0301@gmail.com>
2021-06-01 02:29:35 +0900

    refactor: Update OAuthAttributes.java
    Change Users role GUEST to USER in toEntity method.

--- src/main/java/com/lnoah/portfolio/springboot/config/auth/dto/OAuthAttributes.java
+++ src/main/java/com/lnoah/portfolio/springboot/config/auth/dto/OAuthAttributes.java
@@ -45,7 +45,7 @@ public class OAuthAttributes {
                 .name(name)
                 .email(email)
                 .picture(picture)
-                .role(Role.GUEST)
+                .role(Role.USER)
                 .build();
     }
 }
